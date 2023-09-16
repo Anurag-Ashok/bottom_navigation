35c2e28aecccf985896a2e4b310638d7e1708689
Anurag Ashok <131293260+Anurag-Ashok@users.noreply.github.com>
2023-09-16 21:48:07 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,9 @@
+import 'package:bottomnavigation/bottom.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: bottom(),
+  ));
+}
